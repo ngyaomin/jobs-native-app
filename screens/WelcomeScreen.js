15b05c27f8a0fd6b3aecdcbc00ddb19701042fc9
Yao Min <ngyaomin@ymail.com>
2017-07-02 20:56:05 +0800

    Add background color styling

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -3,8 +3,9 @@ import { View, Text } from 'react-native';
 import Slides from '../components/Slides';
 
 const SLIDE_DATA = [
-  { text: 'Welcome to Job App'},
-  { text: 'set location and swap away to your next money' }
+  { text: 'Welcome to Job App', color: '#03a9f4' },
+  { text: 'get ur dream job', color: '#009688' },
+  { text: 'set location and swap away to your next money', color: '#03a9f4' }
 ];
 
 class WelcomeScreen extends Component {
