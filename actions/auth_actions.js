70f481a80a2fd69cd955707139ee66e418c94364
Yao Min <ngyaomin@ymail.com>
2017-07-09 20:27:39 +0800

    Facebook Login

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,14 +1,29 @@
 import { AsyncStorage } from 'react-native';
+import { Facebook } from 'expo';
 
 import {
-  FACEBOOK_LOGIN_SUCCESS
+  FACEBOOK_LOGIN_SUCCESS,
+  FACEBOOK_LOGIN_FAIL
 } from './types';
 
 export const facebookLogin = () => async dispatch => {
   let token = await AsyncStorage.getItem('fb_token');
   if (token) {
-
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
   } else {
-
+    doFacebookLogin(dispatch);
   }
-}
+};
+
+const doFacebookLogin = dispatch => {
+  let { type, token } = await Facebook.logInWithReadPermissionsAsync('app id here',{
+    permissions: ['public_profile']
+  });
+
+  if (type === 'cancel') {
+    return dispatch({ type: FACEBOOK_LOGIN_FAIL })
+  }
+
+  await AsyncStorage.setItem('fb_token', token);
+  dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+};
