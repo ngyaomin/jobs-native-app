2b91226cb627f920a7652221f1f2f2a6d8f41317
Yao Min <ngyaomin@ymail.com>
2017-07-15 17:35:00 +0800

    Setting up the render mapviews

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import { connect } from 'react-redux';
 import { MapView } from 'expo';
 import { Card, Button } from 'react-native-elements';
@@ -7,8 +7,25 @@ import  Swipe from '../components/Swipe';
 
 class DeckScreen extends Component {
   renderCard(job) {
+    const initialRegion = {
+      longitude: job.longitude,
+      latitude: job.latitude,
+      latitudeDelta: 0.045,
+      longitudeDelta: 0.02
+    };
+
     return (
       <Card title={job.jobtitle}>
+        <View style={{ height: 300 }}>
+          <MapView
+            scrollEnabled={false}
+            style={{ flex: 1 }}
+            cacheEnabled={Platform.OS === 'andriod' ? true : false}
+            initialRegion={initialRegion}
+          >
+          </MapView>
+        </View>
+
         <View style={styles.detailWrapper}>
           <Text>{job.company}</Text>
           <Text>{job.formattedRelativeTime}</Text>
