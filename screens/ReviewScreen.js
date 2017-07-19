0966d7d606b03d32b5a63367bc723d6d864aa6b7
Yao Min <ngyaomin@ymail.com>
2017-07-19 20:49:32 +0800

    Getting started on the review screen

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, Platform } from 'react-native';
 import { Button } from 'react-native-elements';
+import { connect } from 'react-redux';
 
 class ReviewScreen extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -34,4 +35,10 @@ class ReviewScreen extends Component {
   }
 }
 
-export default ReviewScreen;
+function mapStateToProps(state) {
+  return {
+    likedJobs: state.likedJobs
+  };
+}
+
+export default connect(mapStateToProps)(ReviewScreen);
