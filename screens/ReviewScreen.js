c24a2ab8b13d968e6f241ef0a2059d1579cbc65e
Yao Min <ngyaomin@ymail.com>
2017-07-19 21:02:20 +0800

    Debug

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, Platform, ScrollView } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Button, Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 
 class ReviewScreen extends Component {
